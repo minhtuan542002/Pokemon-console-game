afee79598cde51c12a735f66dfff170344b8e651
Riordan Dervin Alfredo <riordan.alfredo@gmail.com>
2022-08-13 15:48:45 +1000

    refactor locations + add game diagram

--- src/game/behaviours/FollowBehaviour.java
+++ src/game/behaviours/FollowBehaviour.java
@@ -6,7 +6,6 @@ import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
 import edu.monash.fit2099.engine.actions.MoveActorAction;
-import game.Behaviour;
 
 /**
  * A class that figures out a MoveAction that will move the actor one step 
--- src/game/behaviours/WanderBehaviour.java
+++ src/game/behaviours/WanderBehaviour.java
@@ -8,7 +8,6 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
-import game.Behaviour;
 
 public class WanderBehaviour implements Behaviour {
 	
