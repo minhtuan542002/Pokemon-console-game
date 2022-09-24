79027f5eadb50089e7d34802b330d36abb4c81b7
minhtuan542002 <minhtuan542002@gmail.com>
2022-09-24 21:39:43 +1000

    Create attributes and methods for CaptureAction class

--- src/game/action/CaptureAction.java
+++ src/game/action/CaptureAction.java
@@ -1,4 +1,36 @@
 package game.action;
 
-public class CaptureAction {
+import javax.swing.*;
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.pokemons.Pokemon;
+
+import java.util.Random;
+
+public class CaptureAction extends Action {
+
+    /**
+     * The Pokemon that is to be captured
+     */
+    protected Pokemon target;
+
+    /**
+     * The direction of the Pokemon that is to be captured.
+     */
+    protected String direction;
+
+    /**
+     * Random number generator
+     */
+    protected Random rand = new Random();
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " tries to capture " + target + " at " + direction;
+    }
 }
--- src/game/time/TimePerceptionManager.java
+++ src/game/time/TimePerceptionManager.java
@@ -17,6 +17,7 @@ import java.util.List;
  * Created by:
  * @author Riordan D. Alfredo
  * Modified by:
+ * @author Minh Tuan Le
  *
  */
 public class TimePerceptionManager {
