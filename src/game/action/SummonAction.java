2d0d4d68f88c55619f0174f38e058bc174860ddf
minhtuan542002 <minhtuan542002@gmail.com>
2022-09-26 07:30:43 +1000

    Implement SummonAction class and add it to Pokeball action list

--- src/game/action/SummonAction.java
+++ src/game/action/SummonAction.java
@@ -2,7 +2,9 @@ package game.action;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
 import game.items.Pokeball;
 import game.pokemons.Pokemon;
 
@@ -15,9 +17,28 @@ public class SummonAction extends Action {
      */
     protected Pokeball summon;
 
+    public SummonAction(Pokeball pokeball) {
+        this.summon = pokeball;
+    }
+
     @Override
     public String execute(Actor actor, GameMap map) {
-        return null;
+        Location summonSpot = null;
+        for (Exit exit : map.locationOf(actor).getExits()) {
+            if(exit.getDestination().canActorEnter(summon.getPokemon())){
+                summonSpot = exit.getDestination();
+                break;
+            }
+        }
+        if(summonSpot == null) {
+            return "There are no space next to you to summon the Pokemon!";
+        }
+        else {
+            summonSpot.addActor(summon.getPokemon());
+            actor.removeItemFromInventory(summon);
+            summon.remove();
+            return summon.getPokemon() + "has been summoned!";
+        }
     }
 
     @Override
