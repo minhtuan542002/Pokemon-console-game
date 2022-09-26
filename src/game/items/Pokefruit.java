d3e7dacde091ecf85b65e0e373d7756cdc88bc4d
minhtuan542002 <minhtuan542002@gmail.com>
2022-09-26 21:49:42 +1000

    Import classes in Pokefruit

--- src/game/items/Pokefruit.java
+++ src/game/items/Pokefruit.java
@@ -2,6 +2,11 @@ package game.items;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.Location;
+import game.Status;
+import game.action.FeedPokefruitAction;
+import game.elements.Element;
 import game.trades.Tradable;
 
 public class Pokefruit extends Item implements Tradable {
@@ -15,9 +20,7 @@ public class Pokefruit extends Item implements Tradable {
 
     /***
      * Constructor.
-     *  @param name the name of this Item
-     * @param displayChar the character to use to represent this item if it is on the ground
-     * @param portable true if and only if the Item can be picked up
+     *  @param pokeFruitType The element of the Pokefruit
      */
     public Pokefruit(Element pokeFruitType) {
         super("Pokefruit", 'f', true);
