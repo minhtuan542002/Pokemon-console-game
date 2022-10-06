cd43ba4bb4a641c6be64be7dacbbbe95d3d68524
Minh Tuan Le <mlee0096@student.monash.edu>
2022-10-06 17:19:47 +1100

    Overwrite Squirtle playTurn method to add weapon items when standing near Fire element pokemon

--- src/game/pokemons/Squirtle.java
+++ src/game/pokemons/Squirtle.java
@@ -1,9 +1,20 @@
 package game.pokemons;
 
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
+import game.action.AttackAction;
 import game.elements.Element;
+import game.elements.ElementsHelper;
 import game.specialattacks.BackupWeapons;
 import game.specialattacks.Bubble;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Pokemon Squirtle
@@ -22,6 +33,16 @@ public class Squirtle extends Pokemon{
         backupWeapon = new BackupWeapons(new Bubble());
     }
 
+    @Override
+    public Action playTurn (ActionList actions, Action lastAction, GameMap map, Display display) {
+
+        Actor targetedActor = null;
+        for (Exit exit : map.locationOf(this).getExits()) {
+            targetedActor = map.getActorAt(exit.getDestination());
+            toggleWeapon((targetedActor != null) && (targetedActor.findCapabilitiesByType(Element.class).contains(Element.FIRE)));
+        }
+        return super.playTurn(actions, lastAction, map, display);
+    }
 
     @Override
     protected IntrinsicWeapon getIntrinsicWeapon() {
